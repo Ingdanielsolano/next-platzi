4804ce0134a060b2219a2f84371df8dc286042c1
IngDanielSolano <ds.development.app@gmail.com>
2022-04-24 11:16:46 -0500

    chore(husky): linux

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ export default function Home() {
       <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div className="mx-auto max-w-md">
-          <img src="/img/logo.svg" className="h-6" alt="Tailwind Play" />
           <div className="divide-y divide-gray-300/50">
             <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
               <p>
